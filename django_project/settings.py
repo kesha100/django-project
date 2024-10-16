fa9945613028f0936b4a8002f376b2ab57ca7b38
Aruuke <arukeshxo@gmail.com>
2024-10-16 12:19:11 +0600

    gunicorn

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb_4bz0',
+        'USER': 'user',
+        'PASSWORD': 'z5InTKvdAPHbzwlSGSu78Wxr4JPCw8FZ',
+        'HOST': 'dpg-cs7ljvlumphs73a8kckg-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
