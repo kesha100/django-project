c45a06c2ea4b3bd241fcc4b815ca61bfe6c8f590
Aruuke <arukeshxo@gmail.com>
2024-10-16 12:25:22 +0600

    gunicorn

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9@eo9+_+d!efl2et+^o4zhx#8=&ltx4@y=ktff2nqw%gzdiri1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://django-project-iocr.onrender.com']
 
